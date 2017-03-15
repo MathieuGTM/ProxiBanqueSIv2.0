4eaa0f77ce51653aa6c32ca35f4344dd6c2316e9
mehrziaa <mmerghni39@gmail.com>
2017-03-15 10:29:34 +0100

    les fichiers compte Epargne

--- ProxiBanqueSIv2/src/dao/DaoCompteEpargne.java
+++ ProxiBanqueSIv2/src/dao/DaoCompteEpargne.java
@@ -1,100 +1,88 @@
-	package dao;
+package dao;
 
-	import java.sql.Connection;
-	import java.sql.PreparedStatement;
-	import java.sql.ResultSet;
-	import java.sql.SQLException;
-	import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import metier.CompteEpargne;
 
-public class DaoCompteEpargne implements IdaoCompteEpargne{
-	
-	
-	
-	
-	
-		@Override
-		public void insertCompteEpargne(CompteEpargne compteepargne, Connection cnx) {
-			String sql = "insert into compteEpargnebdd values (seqIdCompte.nextval,?,?,?)";
-
-			try {
-				PreparedStatement pstat = cnx.prepareStatement(sql);
-				pstat.setDouble(1, compteepargne.getSolde());
-				pstat.setString(2, compteepargne.getDateCreation());
-				pstat.setDouble(3, compteepargne.getTaux());
-
-				pstat.executeUpdate();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-
-		}
-
-		@Override
-		public void updateCompteTaux(Connection cnx, int idCompte, double newTaux) {
-
-			String sql = "update comptecourantbdd set decouvert = ?  where idcompte= ?";
-			try {
-				PreparedStatement stat = cnx.prepareStatement(sql);
-				stat.setDouble(1, newTaux);
-				stat.setInt(2, idCompte);
-				stat.executeUpdate();
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-			}
-		}
-
-		@Override
-		public void updateCompteEpargneSolde(Connection cnx, int idCompte, double newSolde) {
-
-			String sql = "update compteEpargnebdd set solde = ?  where idcompte= ?";
-			try {
-				PreparedStatement stat = cnx.prepareStatement(sql);
-				stat.setDouble(1, newSolde);
-				stat.setInt(2, idCompte);
-				stat.executeUpdate();
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-			}
-		}
-
+public class DaoCompteEpargne implements IdaoCompteEpargne {
 
 	@Override
-	public  void deleteCompteEpargne(Connection cnx, int idCompte) {
-	    String sql = "delete from comptecourantbdd where idcompte = ?";
-	    try {
-	        PreparedStatement stat = cnx.prepareStatement(sql);
-	        stat.setInt(1, idCompte);
-	        stat.execute();
-	    } catch (Exception e) {
-	        System.out.println(e.getMessage());
-	    }
+	public void insertCompteEpargne(CompteEpargne compteepargne, Connection cnx) {
+		String sql = "insert into compteEpargnebdd values (seqIdCompte.nextval,?,?,?)";
+
+		try {
+			PreparedStatement pstat = cnx.prepareStatement(sql);
+			pstat.setDouble(1, compteepargne.getSolde());
+			pstat.setString(2, compteepargne.getDateCreation());
+			pstat.setDouble(3, compteepargne.getTaux());
+
+			pstat.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
-
-
 
 	@Override
-	public  int selectIdComptebyName(Connection cnx, String name) {
-	    int idCompte = 0;
-	    try {
-	        String sql = "select idcompte from CompteEpargnebdd where upper(name) =upper(?)";
-	        PreparedStatement stat = cnx.prepareStatement(sql);
-	        stat.setString(1, name);
-	        ResultSet res = stat.executeQuery();
-	        while (res.next()) {
-	            idCompte = res.getInt("idcompte");
-	        }
-	    } catch (Exception e) {
-	        System.out.println(e.getMessage());
+	public void updateCompteTaux(Connection cnx, int idCompteEpargne, double newTaux) {
 
-	    }
-	    return  idCompte;   
+		String sql = "update comptecourantbdd set decouvert = ?  where idcompte= ?";
+		try {
+			PreparedStatement stat = cnx.prepareStatement(sql);
+			stat.setDouble(1, newTaux);
+			stat.setInt(2, idCompteEpargne);
+			stat.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
+	@Override
+	public void updateCompteEpargneSolde(Connection cnx, int idCompteEpargne, double newSolde) {
 
+		String sql = "update compteEpargnebdd set solde = ?  where idcompte= ?";
+		try {
+			PreparedStatement stat = cnx.prepareStatement(sql);
+			stat.setDouble(1, newSolde);
+			stat.setInt(2, idCompteEpargne);
+			stat.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
+	@Override
+	public void deleteCompteEpargne(Connection cnx, int idCompteEpargne) {
+		String sql = "delete from comptecourantbdd where idcompteEpargne = ?";
+		try {
+			PreparedStatement stat = cnx.prepareStatement(sql);
+			stat.setInt(1, idCompteEpargne);
+			stat.execute();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
+	@Override
+	public int selectIdComptebyName(Connection cnx, String name) {
+		int idCompteEpargne = 0;
+		try {
+			String sql = "select idcompte from CompteEpargnebdd where upper(name) =upper(?)";
+			PreparedStatement stat = cnx.prepareStatement(sql);
+			stat.setString(1, name);
+			ResultSet res = stat.executeQuery();
+			while (res.next()) {
+				idCompteEpargne = res.getInt("idcompteEpargne");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+
+		}
+		return idCompteEpargne;
+	}
 
 }
--- ProxiBanqueSIv2/src/dao/IdaoCompteEpargne.java
+++ ProxiBanqueSIv2/src/dao/IdaoCompteEpargne.java
@@ -5,16 +5,15 @@ import java.sql.Connection;
 import metier.CompteEpargne;
 
 public interface IdaoCompteEpargne {
-	
-	
-	public void insertCompteEpargne(CompteEpargne compteepargne, Connection cnx); 
-	public void updateCompteTaux(Connection cnx, int idCompte, double newTaux);
+
+	public void insertCompteEpargne(CompteEpargne compteepargne, Connection cnx);
+
+	public void updateCompteTaux(Connection cnx, int idCompteEpargne, double newTaux);
+
 	public void updateCompteEpargneSolde(Connection cnx, int idCompte, double newSolde);
 
-	public  void deleteCompteEpargne(Connection cnx, int idCompte);
-	public  int selectIdComptebyName(Connection cnx, String name);
-		  
-		
-		
+	public void deleteCompteEpargne(Connection cnx, int idCompteEpargne);
+
+	public int selectIdComptebyName(Connection cnx, String name);
 
 }
