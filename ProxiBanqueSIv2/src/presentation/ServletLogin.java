907b869bb97536ddf0ebd70dde96ed2a3480e465
aurelie <aurelie.potier@gmail.com>
2017-03-15 09:51:12 +0100

    change ServletLogin

--- ProxiBanqueSIv2/src/presentation/ServletLogin.java
+++ ProxiBanqueSIv2/src/presentation/ServletLogin.java
@@ -1,13 +1,11 @@
+package presentation;
+
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import javax.servlet.http.*;
 import metier.Conseiller;
 
 /**
@@ -28,7 +26,7 @@ public class ServletLogin extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		doPost(request, response);
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
